d359f47f23816ed636fef996d071592c9ff6b386
laker007 <838205692@qq.com>
2017-08-23 16:58:11 +0800

    get openid and session_key

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+
+var AppID = 'wx8af32e262b3f0544';
+var AppSecret = 'e86f936bf32dc4a84a01d910a37338df';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+})
+
+router.post('/', function (req, res, next) {
+  var js_code = req.body.js_code;
+  var encryptedData = req.body.encryptedData;
+  var iv = req.body.iv;
+  var result;
+  var _res = res;
+
+  console.log(js_code)
+  console.log(encryptedData);
+  console.log(iv);
+
+  console.log(AppID);
+  console.log(AppSecret);
+
+  var url = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + AppID + '&secret=' + AppSecret + '&js_code=' + js_code + '&grant_type=authorization_code'
+  console.log(url);
+  //get 请求外网  
+  https.get(url, function (res) {
+
+    res.on('data', function (data) {
+        const buf = Buffer.from(data);
+        // 包含 session_key expires_in openid
+        result = JSON.parse(buf.toString());
+      })
+      .on('end', function () {
+        // Todo: result 不应该传到前端去
+        _res.json(result);
+      })
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+// var User = require('../models/user');
+// find user
+/**
+ *     User.find(function (err, users) {
+      if (err) {
+        res.send(err);
+      }
+
+      console.log(res.json(users))
+    })
+
+        var user = new User(); // create a new instance of the Bear model
+    user.name = req.params.name; // set the bears name (comes from the request)
+
+    user.save(function (err) {
+      if (err)
+        res.send(err);
+
+      res.json({
+        message: 'User created!'
+      });
+    });
+
+        User.remove({
+      _id: req.params.name
+    }, function (err, user) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({
+        message: 'Successfully deleted'
+      });
+    })
+ */
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
