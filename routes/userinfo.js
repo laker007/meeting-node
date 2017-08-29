e1246a22ef21febd2b3344ce02911cca6388ccd9
laker007 <838205692@qq.com>
2017-08-29 18:05:43 +0800

    update

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,22 +1,48 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.model');
+var Company = require('../models/company.model');
+var Department = require('../models/department.model');
+var Team = require('../models/team.model');
 
 router.get('/', function (req, res, next) {
-    var _user = new User();
+    var _user = {
+        OpenID: null,
+        CompanyName: null,
+        DepartmentName: null,
+        TeamName: null,
+    };
 
-    console.log(req.query.OpenID);
+    console.log('get userinfo ' + req.query.OpenID);
     User.findOne({
         OpenID: req.query.OpenID
     }, function (err, user) {
-        console.log(user);
+        if (err) throw err;
 
         // 查找公司
-        User.findOne({
-            CompanyID: user.CompanyID
+        Company.findOne({
+            _id: user.CompanyID
         }, function (err, company) {
-            _user.CompanyName = company.CompanyName;
+            console.log(company);
+            // _user.CompanyName = company.CompanyName
         })
+        // 查找部门
+        Department.findOne({
+            _id: user.DepartmentID
+        }, function (err, department) {
+            console.log(department);
+            // _user.DepartmentName = department.DepartmentName;
+        })
+        // 查找团队
+        Team.findOne({
+            _id: user.TeamID
+        }, function (err, team) {
+            console.log(team);
+            // _user.TeamName = team.TeamName;
+        })
+
+        res.json(_user);
+
     })
 })
 
